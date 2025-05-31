f43fa3d0dac7397969d70add6f109cfe2a5ef5a1
鯨井裕介 <88263476+jakepeter@users.noreply.github.com>
2025-05-31 17:12:54 +0900

    Add positive messages app for Render deployment

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 import random
+import os
 
 app = Flask(__name__)
 
@@ -115,4 +116,5 @@ def index():
     return render_template('index.html', message=message)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
